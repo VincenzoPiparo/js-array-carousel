c3603be9c506b5d21c7da01c3bfa11eb85213291
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-18 18:10:51 +0100

    MILESTONE 1

--- MILESTONE  1/js/script.js
+++ MILESTONE  1/js/script.js
@@ -1,3 +1,9 @@
+// MILESTONE 2
+// Adesso rimuoviamo tutto il markup statico e inseriamo le immagini e i testi delle 
+// immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che 
+// concatena un template literal. Al termine di questa fase ci ritroveremo con lo
+//  stesso slider, ma costruito dinamicamente attraverso JavaScript.
+
 const items = [
     'img/01.jpg',
     'img/02.jpg',
